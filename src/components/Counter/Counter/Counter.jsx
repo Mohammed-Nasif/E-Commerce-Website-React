a4341e61321a6d71a6e1e2b3e246c1002e54aa02
Mohammed Nasif <99568326+Mohammed-Nasif@users.noreply.github.com>
2022-10-19 12:05:40 +0200

    Delete src directory

--- src/components/Counter/Counter/Counter.jsx
+++
@@ -1,7 +0,0 @@
-import { useSelector } from 'react-redux';
-
-export default function Counter() {
-	const count = useSelector((state) => state.counter.counterValue);
-
-	return <h1 className='text-white my-4'>{count}</h1>;
-}
